fd46d50b5ec69b90954c3b7ddc97e8aa23a9de99
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2017-12-07 11:48:18 +0800

    add CharacterControlLogic and Self_ThirdPersonCamera

---
+++ Assets/Scripts/CharacterControlLogic.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControlLogic : MonoBehaviour {
+
+    #region Variables (private)
+
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float directionDampTime = .25f;
+    [SerializeField]
+    private float speedDampTime = .25f;
+
+    private float speed = 0.0f;
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    #endregion
+
+    #region Properties (public)
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        animator = gameObject.GetComponent<Animator>();
+        if (animator.layerCount >= 2) {
+            animator.SetLayerWeight(1, 1);
+        }
+
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (animator) {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+            //speed = new Vector2(h,v).sqrMagnitude;
+            speed = h * h + v * v;
+
+            animator.SetFloat("Speed", speed, speedDampTime, Time.deltaTime);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+
+            if (Input.GetButtonDown("Vertical"))
+            {
+                //Debug.Log("Vertical Key Down");
+                //Debug.Log(speed);
+            }
+            else if (Input.GetButtonDown("Horizontal"))
+            {
+                //Debug.Log("Horizontal Key Down");
+                //Debug.Log(speed);
+            }
+            else if (Input.anyKeyDown)
+            {
+                //Debug.Log("Any Key Down");
+            }
+        }
+
+        //if (Input.GetKeyDown("escape")) {
+        //    Cursor.lockState = CursorLockMode.None;
+        //}
+    }
+
+    /// <summary>
+	/// Debugging information should be put here.
+	/// </summary>
+	void OnDrawGizmos()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Self_ThirdPersonCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Self_ThirdPersonCamera : MonoBehaviour {
+
+    #region Variable (private)
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform follow;
+    private Vector3 targetPosition;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        // notice set the character (Beta) with the tag to the Player
+        follow = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        // set up the position to be the correct offset of the character
+        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+        // making a smooth transition between it's current position and the position it wants to be in
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        // make sure the camera is looking the right way
+        transform.LookAt(follow);
+    }
+}
