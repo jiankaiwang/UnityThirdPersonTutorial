deba5f23dcbf4dece8d826aef81eb5179b4f72c8
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2017-12-11 10:34:12 +0800

    add the compensate for walls
    handling the situation when the camera hits the wall

--- Assets/Scripts/Self_ThirdPersonCamera.cs
+++ Assets/Scripts/Self_ThirdPersonCamera.cs
@@ -13,6 +13,8 @@ public class Self_ThirdPersonCamera : MonoBehaviour {
     private float smooth;
     [SerializeField]
     private Transform follow;
+    [SerializeField]
+    private CharacterControllerLogic followXform;
     private Vector3 targetPosition;
     #endregion
 
@@ -20,7 +22,8 @@ public class Self_ThirdPersonCamera : MonoBehaviour {
     void Start () {
         // notice set the character (Beta) with the tag to the Player
         follow = GameObject.FindWithTag("Player").transform;
-	}
+        followXform = GameObject.FindWithTag("Player").GetComponent<CharacterControllerLogic>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,16 +32,34 @@ public class Self_ThirdPersonCamera : MonoBehaviour {
 
     private void LateUpdate()
     {
+        Vector3 characterOffSet = follow.position + new Vector3(0f, distanceUp, 0f);
+
         // set up the position to be the correct offset of the character
         targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
         Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
         Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
         Debug.DrawLine(follow.position, targetPosition, Color.magenta);
 
-        // making a smooth transition between it's current position and the position it wants to be in
+        // first is to check whether camera is against the wall or not
+        CompensateForWalls(characterOffSet, ref targetPosition);
+        // if not, making a smooth transition between it's current position and the position it wants to be in
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
 
         // make sure the camera is looking the right way
         transform.LookAt(follow);
     }
+
+    #region methods
+    private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget)
+    {
+        Debug.DrawLine(fromObject, toTarget, Color.cyan);
+        // Compensate for walls between camera
+        RaycastHit wallHit = new RaycastHit();
+        if (Physics.Linecast(fromObject, toTarget, out wallHit)) {
+            Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
+            // move the camera to the hitting position
+            toTarget = new Vector3(wallHit.point.x, toTarget.y, wallHit.point.z);
+        }
+    }
+    #endregion
 }
